99c94211d2352073f7396ade44798fd6311f8ef3
Nitish Dubey <nitishd@hlbho.hongleong.com.my>
2022-07-20 23:05:33 +0800

    constraint Layout basics

--- app/src/main/java/com/example/dogprofilepage/ProfilePage.kt
+++ app/src/main/java/com/example/dogprofilepage/ProfilePage.kt
@@ -18,6 +18,8 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 
 
 @Composable
@@ -33,10 +35,9 @@ fun ProfilePage()
          * Modifier.fillMaxSize take the entire row to fill
          * Content of our card
          */
-        Column (Modifier.verticalScroll(rememberScrollState()),
-            horizontalAlignment = Alignment.CenterHorizontally,
-           verticalArrangement = Arrangement.Center)
-        {
+        ConstraintLayout{
+            val (image,cutePuppies,cityText,cutePuppisDec,buttonFollow,buttonDirectMessge)=createRefs()
+            val guideLine=createGuidelineFromTop(0.1f)
             Image(painter = painterResource(id = R.drawable.dog_img)
                 ,contentDescription = "Puppies"
                 , modifier = Modifier
@@ -44,36 +45,67 @@ fun ProfilePage()
                     .clip(CircleShape)
                     .border(
                         width = 2.dp, color = Color.Blue, shape = CircleShape
-                    )
+                    ).constrainAs(image) // How something is positioned
+                    {
+                        top.linkTo(guideLine) // it is linked at the top of the parent card
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+
+                    }
                 ,contentScale = ContentScale.Crop)
-            Text(text="Cutest puppies")
-            Text(text="Kuala Lumpur")
+            Text(text="Cutest puppies",
+            modifier= Modifier.constrainAs(cutePuppies)
+            {
+                top.linkTo(image.bottom) // it will be under image
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            })
+            Text(text="Kuala Lumpur",
+            modifier = Modifier.constrainAs(cityText)
+            {
+                top.linkTo(cutePuppies.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            })
 
             Row(horizontalArrangement = Arrangement.SpaceEvenly,
                 modifier= Modifier
                     .fillMaxWidth()
-                    .padding(16.dp))
+                    .padding(16.dp)
+                    .constrainAs(cutePuppisDec)
+                    {
+                        top.linkTo(cityText.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    })
             {
                 ProfileStats("150","Followers")
                 ProfileStats("100","Following")
                 ProfileStats("30","Post")
             }
 
-            Row(horizontalArrangement = Arrangement.SpaceEvenly,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp))
-            {
                 Button(onClick = { /*TODO*/ },colors = ButtonDefaults.buttonColors(
-                    Color.DarkGray)) {
+                    Color.DarkGray),modifier = Modifier.constrainAs(buttonFollow)
+                {
+                    top.linkTo(cutePuppisDec.bottom,margin = 16.dp)
+                    start.linkTo(parent.start)
+                    end.linkTo(buttonDirectMessge.start)
+                    width= Dimension.wrapContent
+                }) {
                     Text(text = "Follow User",color = Color.White)
 
                 }
-                Button(onClick = { /*TODO*/ }) {
+                Button(onClick = { /*TODO*/ },modifier=Modifier.constrainAs(buttonDirectMessge)
+                {
+                    top.linkTo(cutePuppisDec.bottom,margin = 16.dp)
+                    start.linkTo(buttonFollow.end,margin = 16.dp)
+                    end.linkTo(parent.end)
+                    width= Dimension.wrapContent
+                }) {
                     Text(text = "Direct Message")
 
                 }
-            }
+
         }
     }
 
